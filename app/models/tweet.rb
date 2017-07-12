fb5838ad9efa8a0ea650246aeb81b6ed24f9e387
Julie <jevans6363@gmail.com>
2017-07-12 18:50:30 -0400

    Added validations to tweet and user models.

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,7 @@
 class Tweet < ApplicationRecord
   belongs_to :user
+
+  validates :message, presence: true
+  validates :message, length: {maximum: 140, 
+  too_long: "Only 140 characters! Keep it simple stupid!"}
 end
